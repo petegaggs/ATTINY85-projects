0280fc3b93338256694e259654fbb59af14b2688
petegaggs <35112776+petegaggs@users.noreply.github.com>
2019-02-10 16:03:49 +0000

    Add files via upload

---
+++ tiny_noise.ino
@@ -0,0 +1,35 @@
+/*
+ * Noise generator for ATTINT85
+ * Uses 32 bit LFSR, output on PB0 (pin 5 of IC)
+ */
+
+#include <avr/io.h> 
+
+uint32_t lfsr = 1; //32 bit LFSR, must be non-zero to start
+
+void setup() {
+  pinMode(0,OUTPUT); // this pins gives out the noise
+  // Set up timer 1 to generate interrupt at 31.25kHz
+  TCCR1 = _BV(CS10);
+  TIMSK = _BV(TOIE1);
+}
+
+void loop() {
+}
+
+SIGNAL(TIMER1_OVF_vect) {
+  // set or clear noise pin PB0
+  unsigned lsb = lfsr & 1;
+  if (lsb) {
+    PORTB |= 1;
+  }
+  else {
+    PORTB &= ~1;
+  }
+  // advance LFSR
+  lfsr >>= 1;
+  if (lsb) {
+    lfsr ^= 0xA3000000u;
+  }
+}
+
